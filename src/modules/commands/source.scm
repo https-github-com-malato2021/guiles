589ca8569ada5b991798fa6cfc1a3bc2134f20ce
Rob Bolton <rab26@aber.ac.uk>
2017-02-08 17:33:43 +0000

    Added source command.

---
+++ src/modules/commands/source.scm
@@ -0,0 +1,28 @@
+;; Guiles, a simple IRC bot written in Guile Scheme
+;; Copyright (C) 2017 Robert Bolton
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (commands source))
+
+(use-modules (core irc))
+
+(define src-uri "https://github.com/rob-a-bolton/guiles")
+
+(define (handle-source con usr chan args)
+  (when (= 0 (length args))
+    (say con chan (format #f "~a: ~a" (user-nick usr) src-uri))))
+
+(define hooks
+  `((command-source . ,handle-source)))
